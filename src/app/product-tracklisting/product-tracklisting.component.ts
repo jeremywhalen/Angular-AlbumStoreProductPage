2bad8ce858cd8337f90b6bc7d958a2a673cbba73
Whalen, Jeremy <jmwhalen@firstcommand.com>
2020-08-31 12:47:10 -0500

    Module 5 commit 1

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Album } from '../album';
+import { ProductService } from '..product.service';
+
+<div class="albumInfo">
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
